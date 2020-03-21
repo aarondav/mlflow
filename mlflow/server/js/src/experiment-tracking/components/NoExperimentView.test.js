0a5843069b26ecf763893733abc8216fadadf4d4
Richard Zang <Zangr@users.noreply.github.com>
2020-03-20 17:00:28 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 0bb9a024bb325229b5c7325e01c18a4f44b2850a

---
+++ mlflow/server/js/src/experiment-tracking/components/NoExperimentView.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { NoExperimentView } from './NoExperimentView';
+
+describe('NoExperimentView', () => {
+  test('should render without exploding', () => {
+    shallow(<NoExperimentView/>);
+  });
+});
+
---
+++ mlflow/server/js/src/experiment-tracking/components/PageNotFoundView.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { PageNotFoundView } from './PageNotFoundView';
+
+describe('PageNotFoundView', () => {
+  test('should render without exploding', () => {
+    shallow(<PageNotFoundView/>);
+  });
+});
+
