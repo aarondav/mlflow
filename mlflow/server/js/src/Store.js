a68b936e280ebed44261ea09a11fec277dd5b88e
Richard Zang <Zangr@users.noreply.github.com>
2020-03-21 22:26:04 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: 7a1079bd823a4a7165a2a182f61c76369b5b7817

--- mlflow/server/js/src/Store.js
+++ mlflow/server/js/src/Store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
 
-import { rootReducer } from './reducers/Reducers';
+import { rootReducer } from './experiment-tracking/reducers/Reducers';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
